65f89769ab499474b73da47c5d114c0852ab44a1
Ayush Singh <ayushfullstack@gmail.com>
2026-06-08 13:43:15 +0530

    feat(backend): add user routes (register, login, admin, profile)

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { loginUser, registerUser, adminLogin, getProfile } from '../controllers/userController.js';
+import authUser from '../middleware/auth.js';
+
+const userRouter = express.Router();
+
+userRouter.post('/register', registerUser)
+userRouter.post('/login', loginUser)
+userRouter.post('/admin', adminLogin)
+userRouter.post('/profile', authUser, getProfile)
+
+export default userRouter;
